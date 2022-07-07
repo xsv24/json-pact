3eb66a5dd8b491b1361098ea75e5e35f6fac9af5
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-07-07 21:11:03 +1200

    Add inital System.Text.Json template.

---
+++ src/JsonPact/System/Casing.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using JsonPact;
+
+namespace JsonPact.System;
+
+public class NamingStrategy : JsonNamingPolicy {
+    private readonly JsonPactCase _casing;
+
+    public NamingStrategy(JsonPactCase casing) {
+        _casing = casing;
+    }
+
+    public override string ConvertName(string name) => name.IntoCasedStr(_casing);
+}
