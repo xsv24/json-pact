a03bff27b42e324586a814e5233adda2fdae48bc
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-07-19 16:08:08 +1200

    Centrialize deps & support multiple dotnet versions. (#10)
    * Centalize packages and support net 5.0
    
    * try to use file_scoped.
    
    * Update github actions config.
    
    * Try dotnet 3.1
    
    * Fix up build.
    
    * fmt.
    
    * Try dotnet 3 only.
    
    * Temp disable the lint step.
    
    * Try just dotnet 3.1
    
    * Remove lang version.
    
    * Revert "Remove lang version."
    
    This reverts commit 2bf3862aa30850cf742f5f24cd39cad6a8c05e13.
    
    * Revert "Try just dotnet 3.1"
    
    This reverts commit 2d6998ce9029f461c58e111b1f2949567138546a.
    
    * Revert "Temp disable the lint step."
    
    This reverts commit dd782240870aad1b2f5158e89cc7d4c7113b5ed0.
    
    * Revert "Try dotnet 3 only."
    
    This reverts commit 2d5c3140dfd112996773a2a2fe6fd85946a4c7a4.
    
    * Make fully dotnet 5 compatable.
    
    * clean up test structs.

--- src/JsonPact/Exceptions.cs
+++ src/JsonPact/Exceptions.cs
@@ -2,14 +2,18 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-public abstract class JsonPactException : Exception {
-    public JsonPactException(string message, Exception? inner = null) : base(message, inner) { }
-}
+using System;
 
-public class JsonPactDecodeException : JsonPactException {
-    public JsonPactDecodeException(string message, Exception? inner = null) : base(message, inner) { }
-}
+namespace JsonPact {
+    public abstract class JsonPactException : Exception {
+        protected JsonPactException(string message, Exception? inner = null) : base(message, inner) { }
+    }
 
-public class JsonPactEncodeException : JsonPactException {
-    public JsonPactEncodeException(string message, Exception? inner = null) : base(message, inner) { }
+    public class JsonPactDecodeException : JsonPactException {
+        public JsonPactDecodeException(string message, Exception? inner = null) : base(message, inner) { }
+    }
+
+    public class JsonPactEncodeException : JsonPactException {
+        public JsonPactEncodeException(string message, Exception? inner = null) : base(message, inner) { }
+    }
 }
